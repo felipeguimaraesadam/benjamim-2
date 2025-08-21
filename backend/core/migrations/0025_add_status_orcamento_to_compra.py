59c5a01f583be6e49ce291f98d42d4308cac6cf9
felipeguimaraesadam <idng3@hotmail.com>
2025-08-21 01:48:54 -0300

    Fix: Resolve conflito de migração e atualiza banco de dados do cliente

--- backend/core/migrations/0025_add_status_orcamento_to_compra.py
+++ backend/core/migrations/0025_add_status_orcamento_to_compra.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0024_auto_20250801_0904'),
+        ('core', '0024_compra_status_orcamento_compra_tipo'),
     ]
 
     operations = [
