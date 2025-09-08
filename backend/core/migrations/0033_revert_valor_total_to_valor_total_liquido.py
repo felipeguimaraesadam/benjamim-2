a8ef740f21060a997a479002dfdd77d7f623a527
felipeguimaraesadam <69695547+felipeguimaraesadam@users.noreply.github.com>
2025-09-08 20:59:17 -0300

    Fix: Corrige dependência de migração do Django

--- backend/core/migrations/0033_revert_valor_total_to_valor_total_liquido.py
+++ backend/core/migrations/0033_revert_valor_total_to_valor_total_liquido.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0032_auto_20250827_1318'),
+        ('core', '0032_create_frete_material'),
     ]
 
     operations = [
