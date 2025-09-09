0b588fde821a0de26da3c2c9b9c2a05cea657237
felipeguimaraesadam <69695547+felipeguimaraesadam@users.noreply.github.com>
2025-09-09 19:23:41 -0300

    Configurar migração SQLite para PostgreSQL e integração AWS S3 no Render

---
+++ backend/core/management/commands/load_data.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+class Command(BaseCommand):
+    help = 'Loads data from data.json into the database.'
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS('Loading data from data.json...'))
+        call_command('loaddata', 'data.json')
+        self.stdout.write(self.style.SUCCESS('Data loaded successfully!'))
