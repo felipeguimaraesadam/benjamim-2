a4bd95435203a24ccaefb238dc681c000eb131fa
felipeguimaraesadam <69695547+felipeguimaraesadam@users.noreply.github.com>
2025-09-09 12:21:52 -0300

    fix: Ajuste de CORS

--- backend/sgo_core/settings.py
+++ backend/sgo_core/settings.py
@@ -168,10 +168,9 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    'http://localhost:5174',
-    'http://127.0.0.1:5174',
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://frontend-s7jt.onrender.com",
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_URLS_REGEX = r'^/api/.*$'
